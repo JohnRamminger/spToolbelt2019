97ec9070f960c52bb27a31a09ddbdf4db577fa10
chris.johnson.admin <chris.johnson.admin@SP19DEV>
2021-04-26 13:57:34 -0400

    update

--- spToolbelt2019/Forms/frmSyncNav.Designer.cs
+++ spToolbelt2019/Forms/frmSyncNav.Designer.cs
@@ -38,43 +38,48 @@
             // 
             // txtSourceSite
             // 
-            this.txtSourceSite.Location = new System.Drawing.Point(92, 15);
+            this.txtSourceSite.Location = new System.Drawing.Point(184, 29);
+            this.txtSourceSite.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtSourceSite.Name = "txtSourceSite";
-            this.txtSourceSite.Size = new System.Drawing.Size(555, 20);
+            this.txtSourceSite.Size = new System.Drawing.Size(1106, 31);
             this.txtSourceSite.TabIndex = 0;
-            this.txtSourceSite.Text = "https://ramalyze.sharepoint.com/sites/configuration";
+            this.txtSourceSite.Text = "https://internal.ati.org/cfg";
             // 
             // txtTargetSite
             // 
-            this.txtTargetSite.Location = new System.Drawing.Point(92, 51);
+            this.txtTargetSite.Location = new System.Drawing.Point(184, 98);
+            this.txtTargetSite.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtTargetSite.Name = "txtTargetSite";
-            this.txtTargetSite.Size = new System.Drawing.Size(555, 20);
+            this.txtTargetSite.Size = new System.Drawing.Size(1106, 31);
             this.txtTargetSite.TabIndex = 1;
-            this.txtTargetSite.Text = "https://ramalyze.sharepoint.com";
+            this.txtTargetSite.Text = "https://contracts.ati.org/sites/testnav";
             // 
             // S
             // 
             this.S.AutoSize = true;
-            this.S.Location = new System.Drawing.Point(13, 18);
+            this.S.Location = new System.Drawing.Point(26, 35);
+            this.S.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.S.Name = "S";
-            this.S.Size = new System.Drawing.Size(62, 13);
+            this.S.Size = new System.Drawing.Size(123, 25);
             this.S.TabIndex = 2;
             this.S.Text = "Soruce Site";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 51);
+            this.label1.Location = new System.Drawing.Point(26, 98);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.Size = new System.Drawing.Size(117, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Target Site";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(666, 13);
+            this.button1.Location = new System.Drawing.Point(1332, 25);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 58);
+            this.button1.Size = new System.Drawing.Size(224, 112);
             this.button1.TabIndex = 4;
             this.button1.Text = "Sync Navigation";
             this.button1.UseVisualStyleBackColor = true;
@@ -83,22 +88,25 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(16, 84);
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(32, 162);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(762, 355);
+            this.listBox1.Size = new System.Drawing.Size(1520, 679);
             this.listBox1.TabIndex = 5;
             // 
             // frmSyncNav
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.S);
             this.Controls.Add(this.txtTargetSite);
             this.Controls.Add(this.txtSourceSite);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "frmSyncNav";
             this.Text = "frmSyncNav";
             this.Load += new System.EventHandler(this.frmSyncNav_Load);
---
+++ spToolbelt2019lib/Code/Extensions/extGroup.cs
@@ -0,0 +1,56 @@
+﻿
+using Microsoft.SharePoint.Client;
+using System;
+
+
+namespace spToolbelt2019lib.Code.Extensions
+{
+    public static class extGroup
+    {
+        public static void EnsureUser(this Group grp,Web oWeb,string cLoginName)
+        {
+            if (!grp.HasUser(oWeb,cLoginName))
+            {
+                grp.AddUser(oWeb, cLoginName);
+            }
+        }
+        public static bool HasUser(this Group grp,Web oWeb,string cLoginName)
+        {
+            try
+            {
+                grp.Context.Load(grp, g => g.Users);
+                grp.Context.ExecuteQuery();
+                foreach(User usr in grp.Users)
+                {
+                    if (usr.LoginName==cLoginName)
+                    {
+                        return true;
+                    }
+                }
+                
+            } catch (Exception ex)
+            {
+                throw new Exception("Error finding: " + cLoginName + " - " + ex.Message);
+            }
+
+
+            return false;
+
+        }
+
+        public static void AddUser(this Group grp, Web oWeb, string cLoginName)
+        {
+            try
+            {
+                User oUser = oWeb.EnsureUser(cLoginName);
+                grp.Users.AddUser(oUser);
+                grp.Update();
+                oWeb.Context.ExecuteQuery();
+            } catch(Exception ex)
+            {
+                throw new Exception("Error adding: " + cLoginName + " - " + ex.Message);
+            }
+
+        }
+    }
+}
